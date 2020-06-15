9368ab7ac193cab85a562f4ba415feedbf5d9ee3
Gerard-Szulc <gerszulc05@gmail.com>
2020-06-15 21:11:42 +0200

    Adds interfaces.go, and login function for users

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -2,43 +2,32 @@ package migrations
 
 import (
 	"fintGolangApp/helpers"
-	"github.com/jinzhu/gorm"
+	"fintGolangApp/interfaces"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type User struct {
-	gorm.Model
-	Username string
-	Email    string
-	Password string
-}
-type Account struct {
-	gorm.Model
-	Type    string
-	Name    string
-	Balance uint
-	UserID  uint
-}
-
 func createAccounts() {
 	db := helpers.ConnectDB()
-	users := [2]User{
+	users := &[2]interfaces.User{
 		{Username: "Martin", Email: "martin@martin.com"},
 		{Username: "Michael", Email: "michael@michael.com"},
 	}
 	for i := 0; i < len(users); i++ {
+		// Correct one way
 		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
-		user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
+		user := &interfaces.User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
 		db.Create(&user)
-		account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
+		account := &interfaces.Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
 		db.Create(&account)
 	}
 	defer db.Close()
 }
 
 func Migrate() {
+	User := &interfaces.User{}
+	Account := &interfaces.Account{}
 	db := helpers.ConnectDB()
-	db.AutoMigrate(&User{}, &Account{})
+	db.AutoMigrate(&User, &Account)
 	defer db.Close()
 
 	createAccounts()
